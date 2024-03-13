39cafc562c78a5d41857f59353ecea9967474574
Mohsen Banan <form@mohsen.banan.1.byname.net>
2024-03-12 18:53:52 -0700

    Added getNerdFonts.

---
+++ bin/lcaGetNerdFonts.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+IcmBriefDescription="Install NerdFonts with getnf -- needed by doom-mode-line of Blee"
+
+ORIGIN="
+* Revision And Libre-Halaal CopyLeft -- Part Of ByStar -- Best Used With Blee
+"
+
+####+BEGIN: bx:bash:top-of-file :vc "cvs" partof: "bystar" :copyleft "halaal+brief"
+typeset RcsId="$Id: startOrgPanel.sh,v 1.5 2020-09-01 02:10:50 lsipusr Exp $"
+# *CopyLeft*
+# Copyright (c) 2011 Neda Communications, Inc. -- http://www.neda.com
+# See PLPC-120001 for restrictions.
+# This is a Halaal Poly-Existential intended to remain perpetually Halaal.
+####+END:
+
+__author__="
+* Authors: Mohsen BANAN, http://mohsen.banan.1.byname.net/contact
+"
+
+####+BEGIN: bx:bsip:bash:seed-spec :types "seedActions.bash"
+SEED="
+*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedActions.bash]] | 
+"
+FILE="
+*  /This File/ :: /bisos/git/auth/bxRepos/bisos/bsip4/bin/lcaRcLocalCompat.sh 
+"
+if [ "${loadFiles}" == "" ] ; then
+    /bisos/core/bsip/bin/seedActions.bash -l $0 "$@" 
+    exit $?
+fi
+####+END:
+
+_CommentBegin_
+####+BEGIN: bx:dblock:global:file-insert-cond :cond "./blee.el" :file "/libre/ByStar/InitialTemplates/software/plusOrg/dblock/inserts/topControls.org"
+*      ================
+*  /Controls/ ::  [[elisp:(org-cycle)][| ]]  [[elisp:(show-all)][Show-All]]  [[elisp:(org-shifttab)][Overview]]  [[elisp:(progn (org-shifttab) (org-content))][Content]] | [[file:Panel.org][Panel]] | [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] | [[elisp:(bx:org:run-me)][Run]] | [[elisp:(bx:org:run-me-eml)][RunEml]] | [[elisp:(delete-other-windows)][(1)]] | [[elisp:(progn (save-buffer) (kill-buffer))][S&Q]]  [[elisp:(save-buffer)][Save]]  [[elisp:(kill-buffer)][Quit]] [[elisp:(org-cycle)][| ]]
+** /Version Control/ ::  [[elisp:(call-interactively (quote cvs-update))][cvs-update]]  [[elisp:(vc-update)][vc-update]] | [[elisp:(bx:org:agenda:this-file-otherWin)][Agenda-List]]  [[elisp:(bx:org:todo:this-file-otherWin)][ToDo-List]] 
+
+####+END:
+_CommentEnd_
+
+_CommentBegin_
+*      ================
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]] CONTENTS-LIST ################
+*  [[elisp:(org-cycle)][| ]]  Notes         :: *[Current-Info:]*  Status, Notes (Tasks/Todo Lists, etc.) [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_moduleDescription {  cat  << _EOF_
+*  [[elisp:(org-cycle)][| ]]  Xrefs         :: *[Related/Xrefs:]*  <<Xref->>  -- External Documents  [[elisp:(org-cycle)][| ]]
+_EOF_
+}
+
+_CommentBegin_
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]]  *Seed Extensions*
+_CommentEnd_
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Imports       :: Prefaces (Imports/Libraries) [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+. ${opBinBase}/opAcctLib.sh
+. ${opBinBase}/opDoAtAsLib.sh
+. ${opBinBase}/lpParams.libSh
+. ${opBinBase}/lpReRunAs.libSh
+
+# /opt/public/osmt/bin/lcnObjectTree.libSh
+. ${opBinBase}/lcnObjectTree.libSh
+
+# PRE parameters
+
+ports=""
+sysType=""
+
+function G_postParamHook {
+    #vis_loadActionEl
+    return 0
+}
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Examples      :: Examples [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+
+function vis_examples {
+    typeset extraInfo="-h -v -n showRun"
+    #typeset extraInfo=""
+    typeset runInfo="-p ri=lsipusr:passive"
+
+    typeset examplesInfo="${extraInfo} ${runInfo}"
+
+    #vis_startObjectGenExamples
+
+    visLibExamplesOutput ${G_myName} 
+  cat  << _EOF_
+$( examplesSeperatorTopLabel "${G_myName}" )
+_EOF_
+
+  # vis_ftoCommonExamples
+  
+  cat  << _EOF_
+$( examplesSeperatorChapter "Get Nerd Fonts" )
+${G_myName} ${extraInfo} -i gitClone_getnf
+${G_myName} ${extraInfo} -i getnf_NerdFontsSymbolsOnly
+${G_myName} ${extraInfo} -i fullUpdate
+_EOF_
+}
+
+noArgsHook() {
+  vis_examples
+}
+
+g_gitBase="/bisos/git/anon/ext/misc"
+g_gitGetnfBase="${g_gitBase}/getnf"
+g_getnf="${g_gitGetnfBase}/getnf"
+
+function vis_gitClone_getnf {
+  G_funcEntry
+  function describeF {  G_funcEntryShow; cat  << _EOF_
+git clone getnf. https://github.com/getnf/getnf -- A better way to install NerdFonts
+_EOF_
+                     }
+  EH_assert [[ $# -eq 0 ]]
+
+  [ -d "${g_gitBase}" ] || mkdir -p "${g_gitBase}"
+  if [ -d "${g_gitGetnfBase}" ] ; then
+    inBaseDirDo "${g_gitGetnfBase}" git pull
+  else
+    inBaseDirDo "${g_gitBase}" git clone https://github.com/getnf/getnf.git
+  fi
+}
+
+function vis_getnf_NerdFontsSymbolsOnly {
+  G_funcEntry
+  function describeF {  G_funcEntryShow; cat  << _EOF_
+Run ${g_getnf} and install NerdFontsSymbolsOnly
+_EOF_
+                     }
+  EH_assert [[ $# -eq 0 ]]
+
+  lpDo ${g_getnf} -i NerdFontsSymbolsOnly
+}
+
+function vis_fullUpdate {
+  G_funcEntry
+  function describeF {  G_funcEntryShow; cat  << _EOF_
+Clone getnf and install NerdFontsSymbolsOnly
+_EOF_
+                     }
+  EH_assert [[ $# -eq 0 ]]
+
+  lpDo vis_gitClone_getnf
+  lpDo vis_getnf_NerdFontsSymbolsOnly
+}
+
+
+
+_CommentBegin_
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]]  *End Of Editable Text*
+_CommentEnd_
+
+####+BEGIN: bx:dblock:bash:end-of-file :type "basic"
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Common        ::  /[dblock] -- End-Of-File Controls/ [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+#+STARTUP: showall
+#local variables:
+#major-mode: sh-mode
+#fill-column: 90
+# end:
+####+END:
